0b4ed6546045da246f14f124cc46e9bd31f16a5c
RomaFranko <59966214+RomaFranko@users.noreply.github.com>
2020-01-17 17:00:24 +0200

    uploaded from PC

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://static.tildacdn.com/tild6664-6566-4536-b630-333434336665/ally-Recovered-cover.png' />
+        </header>
+    )
+}
+
+export default Header;
